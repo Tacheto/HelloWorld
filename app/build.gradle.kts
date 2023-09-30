8bf5c8ee66cc4023d4a6eb2980a7e28cab0b40dd
Tacheto <tacheto69@gmail.com>
2023-09-30 19:06:58 +0200

    añadido readme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,10 +31,11 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("home/sergi/Android/Sdk/platforms/android-28/android.jar"))
-    implementation(files("C:\\Users\\sergi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"))
+    //TODO Para documentar con javadoc el proyecto se debe añadir (descomentar) la siguiente libreria
+    //implementation(files("home/sergi/Android/Sdk/platforms/android-28/android.jar"))
+    //implementation(files("C:\\Users\\sergi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
